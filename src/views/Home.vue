fd2a29e1080decc681e482032890182caa2edbcb
lee <1079811936@qq.com>
2021-07-08 00:04:33 +0800

    初始化aside

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>pig-UI</h1>
       <h2>一个很厉害的UI</h2>
       <p class="actions">
-        <a href="">GitHUb</a>
-        <a href="">开始</a>
+        <a href="https://github.com">GitHUb</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -20,4 +20,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
