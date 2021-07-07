5138ccfc1de96917c28bcdd052888f8d7fc2e077
lee <1079811936@qq.com>
2021-07-08 03:04:13 +0800

    实现aside切换

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,14 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+import { provide } from 'vue';
+
 export default {
   name: "App",
-};
+  setup(){
+    const asideVisible = ref(false)
+    provide('asideVisible',asideVisible)
+  }
+}
 </script>
