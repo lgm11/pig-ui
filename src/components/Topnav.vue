eb1db742e32cac807c4153409bf907a24a70101c
lee <1079811936@qq.com>
2021-07-08 14:33:02 +0800

    完善aside切换逻辑

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleAside">LOGO</div>
+    <div class="logo" >LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside" @click="toggleAside"></span>
   </div>
 </template>
 
@@ -42,9 +43,20 @@ export default {
       margin: 0 1em;
     }
   }
+  > .toggleAside {
+    display: none;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
   @media (max-width:500px) {
     > .menu{display: none;}
     > .logo{margin: 0 auto;}
+    > .toggleAside{display: inline-block;}
   }
 }
 </style>
