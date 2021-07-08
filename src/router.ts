5cff738090b044c44cecffcf532c905c570d8cb8
lee <1079811936@qq.com>
2021-07-08 15:55:39 +0800

    基本完成官网框架

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
+import DocDemo from './components/DocDemo.vue'
 
 const history = createWebHashHistory()
 export const router = createRouter({
@@ -12,6 +13,7 @@ export const router = createRouter({
     routes: [
         { path: '/', component: Home },
         { path: '/doc', component: Doc ,children:[
+            {path:'',component:DocDemo},
             {path:'button',component:ButtonDemo},
             {path:'dialog',component:DialogDemo},
             {path:'tabs',component:TabsDemo},
