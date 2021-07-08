e78ba33216883874dede4c14221d4dc6eba3273a
lee <1079811936@qq.com>
2021-07-08 20:07:15 +0800

    可以切换了

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
-        Switch组件
+        <Switch />
     </div>
 </template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+export default {
+    components:{Switch}
+}
+</script>
