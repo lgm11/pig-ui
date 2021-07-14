c61f3ba9b3389547b85dbe0d0465d3dee8f34524
lee <1079811936@qq.com>
2021-07-14 14:39:37 +0800

    渲染Tabs内容

---
+++ src/lib/Tabs.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-for="(t,index) in title " :key="index">{{t}}</div>
+        <component v-for="(c,index) in defaults" :is="c" :key="index" />
+    </div>
+</template>
+
+<script lang="ts">
+import Tab from './Tab.vue'
+export default {
+    setup(props,context){
+        const defaults = context.slots.default()
+        defaults.forEach((tag)=>{
+            if(tag.type !== Tab){
+                throw new Error('Tabs子标签必须是Tab')
+            }
+        })
+        const title = defaults.map((tag)=>{return tag.props.title})
+        return {defaults,title}
+    }
+}
+</script>
