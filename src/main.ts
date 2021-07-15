276262bab09eb3c5ef0349220ab115e626dd14da
lee <1079811936@qq.com>
2021-07-16 01:03:06 +0800

    事不过三：优化markdown

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import 'github-markdown-css'
+import MarkDown from './components/MarkDown.vue'
 
 
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
+app.component("MarkDown",MarkDown)
--- src/router.ts
+++ src/router.ts
@@ -6,20 +6,20 @@ import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DocDemo from './components/DocDemo.vue'
-import Install from './views/Install.vue'
-import Intro from './views/Intro.vue'
-import GetStarted from './views/GetStarted.vue'
+import MarkDown from './components/MarkDown.vue'
+import { h } from 'vue'
 
 const history = createWebHashHistory()
+const md = filename=>h(MarkDown,{path:`../markdown/${filename}.md`,key:filename})
 export const router = createRouter({
     history: history,
     routes: [
         { path: '/', component: Home },
         { path: '/doc', component: Doc ,children:[
             {path:'',component:DocDemo},
-            {path:'intro',component:Intro},
-            {path:'get-started',component:GetStarted},
-            {path:'install',component:Install},
+            {path:'intro',component:md('intro')},
+            {path:'get-started',component:md('get-started')},
+            {path:'install',component:md('install')},
             {path:'button',component:ButtonDemo},
             {path:'dialog',component:DialogDemo},
             {path:'tabs',component:TabsDemo},
