8f6524ee6f4f31776e14c82265fa032a30d59a30
lee <1079811936@qq.com>
2021-07-16 17:08:14 +0800

    改造ButtonDemo

---
+++ src/components/Button1.demo.vue
@@ -0,0 +1,21 @@
+<demo>
+常规用法
+</demo>
+
+<template>
+  <div>
+    <Button>按钮</Button>
+    <Button theme="button">按钮</Button>
+    <Button theme="link">按钮</Button>
+    <Button theme="text">按钮</Button>
+  </div>
+</template>
+
+<script>
+import Button from "../lib/Button.vue";
+export default {
+     components: {
+    Button
+  }
+}
+</script>
---
+++ src/components/Button2.demo.vue
@@ -0,0 +1,32 @@
+<demo>
+支持size
+</demo>
+
+<template>
+<div>
+  <div>
+     <Button size="big">大按钮</Button>
+     <Button >正常按钮</Button>
+     <Button size="small">小按钮</Button>
+  </div>
+  <div>
+    <Button theme="link" size="big">大按钮</Button>
+    <Button theme="link">正常按钮</Button>
+    <Button size="small" theme="link">小按钮</Button>
+  </div>
+  <div>
+    <Button size="big" theme="text">大按钮</Button>
+    <Button theme="text">正常按钮</Button>
+    <Button size="small" theme="text">小按钮</Button>
+  </div>
+</div>
+</template>
+
+<script>
+import Button from "../lib/Button.vue";
+export default {
+     components: {
+    Button
+  }
+}
+</script>
