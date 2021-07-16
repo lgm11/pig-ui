d2bc523526d94a07d3ad1f0072b85bb5fa4bd4db
lee <1079811936@qq.com>
2021-07-16 16:50:15 +0800

    删掉无用代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -9,48 +9,18 @@
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
 import { ref } from 'vue'
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
-import 'prismjs'
-import 'prismjs/themes/prism.css'
+
 import Demo from './Demo.vue'
 const Prism = (window as any).Prism
 export default {
-    components:{Button,Demo},
+    components:{Demo},
     setup(){
         const bool = ref(false)
-        return {bool,Switch1Demo,Switch2Demo,Prism}
+        return {Switch1Demo,Switch2Demo}
     }
 }
 </script>
 
-<style lang="scss" scoped>
-$border-color: #d9d9d9;
-.demo {
-  border: 1px solid $border-color;
-  margin: 16px 0 32px;
-  >h2 {
-    font-size: 20px;
-    padding: 8px 16px;
-    border-bottom: 1px solid $border-color;
-  }
-  &-component {
-    padding: 16px;
-  }
-  &-actions {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-  }
-  &-code {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-    >pre {
-      line-height: 1.1;
-      font-family: Consolas, 'Courier New', Courier, monospace;
-      margin: 0;
-    }
-  }
-}
-</style>
