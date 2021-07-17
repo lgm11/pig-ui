b12568e069a597932c5cc2828173acbac84a9383
lee <1079811936@qq.com>
2021-07-17 15:15:02 +0800

    展示或隐藏代码

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,7 +5,9 @@
                 <component :is="component" />
             </div>
             <div class="demo-actions">
-                <Button @click="codeVisible = !codeVisible">查看代码</Button>
+              <Button @click="codeVisible = false" v-if="codeVisible">隐藏代码</Button>
+              <Button @click="codeVisible = true" v-else>查看代码</Button>
+                
             </div>
             <div v-if="codeVisible" class="demo-code">
               <pre class="language-html" v-html="html" />
