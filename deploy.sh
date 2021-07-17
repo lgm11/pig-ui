9323fa1fdf9006f3d70ae7e78c27c1eb01afff6d
lee <1079811936@qq.com>
2021-07-17 15:55:23 +0800

    创建lib/index.ts

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m "update" &&
-git branch -M master &&
+git branch -M main &&
 git remote add origin git@github.com:lgm11/pig-ui-website.git &&
-git push -f -u origin master &&
+git push -f -u origin main &&
 cd -
 echo https://lgm11.github.io/pig-ui-website/index.html
